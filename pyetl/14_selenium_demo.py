7c91904ece548fa22fedd51fca45e631f1f4ee29
uuboyscy <aegis12321@gmail.com>
2024-01-07 21:36:29 +0800

    update

---
+++ pyetl/14_selenium_demo.py
@@ -0,0 +1,50 @@
+from selenium.webdriver import Chrome
+
+# from selenium.webdriver import ChromeOptions
+# from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import requests
+from bs4 import BeautifulSoup
+
+###### Deprecated ######
+# options = ChromeOptions()
+# options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+# print(options.binary_location)
+# driver = Chrome('./chromedriver', options=options)
+# driver = Chrome('./chromedriver')
+########################
+
+# service = Service("./chromedriver")
+# driver = Chrome(service=service)
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+driver = webdriver.Remote(
+    command_executor="http://127.0.0.1:4444/wd/hub",
+    options=chrome_options,
+)
+# driver = Chrome()
+
+url = "https://www.ptt.cc/bbs/index.html"
+
+driver.get(url)
+# driver.find_element_by_class_name('board-name').click() # Deprecated
+driver.find_element(by=By.CLASS_NAME, value="board-name").click()
+# driver.find_element_by_class_name('btn-big').click() # Deprecated
+driver.find_element(by=By.CLASS_NAME, value="btn-big").click()
+
+cookie = driver.get_cookies()
+
+driver.close()
+
+ss = requests.session()
+
+# 設定cookies
+for c in cookie:
+    ss.cookies.set(c["name"], c["value"])
+
+res = ss.get("https://www.ptt.cc/bbs/Gossiping/index.html")
+soup = BeautifulSoup(res.text, "html.parser")
+print(soup.prettify())
